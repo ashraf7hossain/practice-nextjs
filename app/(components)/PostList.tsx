437afba54a120ac41e801075bae58d56629ab8a0
Ashraf Hossain <ashraf7rubby@gmail.com>
2024-10-07 18:57:02 +0600

    added mockapi to mimic crud functionalities

--- app/(components)/PostList.tsx
+++ app/(components)/PostList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./PostList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -14,7 +14,25 @@ function PostList({
 }) {
   const [posts, setPosts] = useState<any>([]);
 
-  function addPostHandler(postData: any) {
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch(
+        "https://6703d4d4ab8a8f892731d5c5.mockapi.io/posts"
+      );
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchPosts();
+  }, [])
+
+  async function addPostHandler(postData: any) {
+    await fetch("https://6703d4d4ab8a8f892731d5c5.mockapi.io/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((posts: any) => [...posts, postData]);
   }
 
@@ -26,11 +44,17 @@ function PostList({
         </Modal>
       )}
       {/* <button onClick={showModal}>Open</button> */}
-      <ul className={classes.posts}>
-        {posts.map((post: any) => (
-          <Post key={post.id} author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 ? (
+        <ul className={classes.posts}>
+          {posts.map((post: any) => (
+            <Post key={post.id} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      ) : (
+        <div>
+          <h2>No posts yet!</h2>
+        </div>
+      )}
     </>
   );
 }
